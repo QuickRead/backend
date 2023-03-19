534c44bf3d7d12fe1fa04eb36083ace0a0946e28
Fassty <mikulaspa@gmail.com>
2023-03-19 17:31:14 +0100

    Add support for loading article bodies and tqdm dependency

--- app/scraper.py
+++ app/scraper.py
@@ -1,3 +1,4 @@
+import time
 from typing import List
 
 from urllib.parse import urljoin
@@ -6,9 +7,18 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+import logging
+from trafilatura import fetch_url, extract
+from tqdm import tqdm
 
+logger = logging.getLogger('root')
 
+# How recent articles to load (default 1 day)
 HISTORY_LENGTH = 60 * 60 * 24 * 1
+# How many articles to return per each source
+RESULT_COUNT = 20
+
+tqdm.pandas()
 
 
 def _get_sitemap_urls(robots_url: str) -> List[str]:
@@ -29,12 +39,13 @@ def get_article_urls_from_web(url: str) -> List[dict]:
 
         if sitemap_xml.find('sitemapindex'):
             sitemaps = sitemap_xml.findAll('sitemap')
-            print(f'Found new {len(sitemaps)} sitemaps...')
+            logger.info(f'Found new {len(sitemaps)} sitemaps...')
             for sitemap in sitemaps:
                 sitemap_urls.append(sitemap.find('loc').text)
         elif sitemap_xml.find('urlset'):
             urls = sitemap_xml.findAll('url')
-            print(f'Processing {len(urls)} URLs...')
+            logger.info(f'Processing {len(urls)} URLs...')
+
             for url in urls:
                 if not url.find('news:news'):
                     # Those are some old weird articles, fuck them :D
@@ -43,7 +54,7 @@ def get_article_urls_from_web(url: str) -> List[dict]:
                 news_info = url.find('news:news')
                 pub_date = news_info.find('news:publication_date').text
                 pub_date = pd.Timestamp(pub_date)
-                title = news_info.find('news:title')
+                title = news_info.find('news:title').text
                 url = url.find('loc').text
 
                 # TODO: alternatively we could do this filtering later, but this will be more memory efficient
@@ -61,9 +72,22 @@ def get_article_urls_from_web(url: str) -> List[dict]:
     return article_urls
 
 
+def load_article_content(article_url: str) -> dict:
+    downloaded = fetch_url(article_url)
+    article = extract(downloaded, favor_precision=True)
+    time.sleep(0.1)
+
+    return article
+
+
+def load_all_atricles(article_df_: pd.DataFrame) -> ...:
+    article_df_['content'] = article_df_['url'].progress_apply(load_article_content)
+    return article_df_
+
+
 if __name__ == '__main__':
     websites = [
-        'https://www.novinky.cz',
+        #'https://www.novinky.cz',
         'https://www.bbc.com',
         'https://www.theguardian.com'
     ]
@@ -71,8 +95,10 @@ if __name__ == '__main__':
     for website_url in websites:
         urls.extend(get_article_urls_from_web(website_url))
 
-    print(f'Loaded a total of {len(urls)}')
+    logger.info(f'Loaded a total of {len(urls)}')
 
     df = pd.DataFrame(urls)
+    df = df.sort_values('timestamp', ascending=False).groupby('source').head(RESULT_COUNT).reset_index(drop=True)
     df.to_csv('articles.csv')
-    print(df.head())
+
+    load_all_atricles(df)
